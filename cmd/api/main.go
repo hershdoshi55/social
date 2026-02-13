c827e23c121650fd061a362a5871e95a3f71bf43
hersh doshi <hershdoshi55@gmail.com>
2026-02-13 04:11:07 -0800

    Setup server and basic routing

---
+++ cmd/api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hershdoshi55/social/internal/env"
+)
+
+
+func main() {
+	cfg := config{
+			addr: env.GetString("ADDR", ":8080"),
+		}
+
+	app := &application{
+		config: cfg,
+	}
+
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
