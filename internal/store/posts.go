a2e653750a8bb4c4a24aa04e3edd3caf5bef9c6c
hersh doshi <hershdoshi55@gmail.com>
2026-02-25 04:09:44 -0800

    implemented adding comments to posts

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -9,13 +9,14 @@ import (
 )
 
 type Post struct {
-	ID        int64    `json:"id"`
-	Content   string   `json:"content"`
-	Title     string   `json:"title"`
-	UserId    int64    `json:"user_id"`
-	Tags      []string `json:"tags"`
-	CreatedAt string   `json:"created_at"`
-	UpdatedAt string   `json:"updated_at"`
+	ID        int64     `json:"id"`
+	Content   string    `json:"content"`
+	Title     string    `json:"title"`
+	UserId    int64     `json:"user_id"`
+	Tags      []string  `json:"tags"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+	Comments  []Comment `json:"comments"`
 }
 type PostStore struct {
 	db *sql.DB
@@ -73,5 +74,5 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		}
 	}
 	return &post, nil
-	
+
 }
